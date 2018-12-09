5815fe75b8847f10388d7b46c37c1452c10b242e
BagboBillbins <33336191+BagboBillbins@users.noreply.github.com>
2018-12-08 18:13:43 -0600

    added all scripts and updated from obsolete load level to use scene manager instead

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+}
